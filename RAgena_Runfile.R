fc4f7b331c9e95c98915333962e1e86653bae68b
Erhan Pisirir <e.pisirir@qmul.ac.uk>
2022-01-31 20:27:52 +0000

    Node objects and parser to define them

---
+++ RAgena_Runfile.R
@@ -0,0 +1,12 @@
+source("RAgena.R")
+
+sw_project_cont_path = "Models/SWProjectRiskCont.cmpx"
+sw_project_disc_path = "Models/SWProjectRiskDiscrete.cmpx"
+car_costs_path = "Models/CarCosts.cmpx"
+adv_rel_model_path = "Models/AdvancedReliabilityModelling.cmpx"
+
+###dev testing
+modelPath <- "Models/AdvancedReliabilityModelling.cmpx"
+modelPath <- "Models/CarCosts.cmpx"
+###
+
